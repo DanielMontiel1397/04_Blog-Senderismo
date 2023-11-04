2b2b0e528cf4d42ad20f72e533ee7990766b29a6
Daniel Montiel <danielponce1397@gmail.com>
2023-11-03 21:53:03 -0600

    Se agrego menu en main de Acerca De

--- script.js
+++ script.js
@@ -3,10 +3,24 @@ const imagenK2= document.querySelector('.imageK2');
 const imagenkachen= document.querySelector('.imageKachen');
 const imagenLhotse= document.querySelector('.imageLhotse');
 
+const queEsSenderismo = document.querySelector('.queEsSenderismo');
+const mountainTallMexico = document.querySelector('.mountainTallMexico');
+const mountainTallWorld = document.querySelector('.mountainTallWorld');
+const escaladoresMasGrandes = document.querySelector('.escaladoresMasGrandes');
+
+const acercaDe1 = document.querySelector('.acercaDe1');
+const acercaDe2 = document.querySelector('.acercaDe2');
+const acercaDe3 = document.querySelector('.acercaDe3');
+const acercaDe4 = document.querySelector('.acercaDe4');
+
 imagenEverest.addEventListener('click', expander1);
 imagenK2.addEventListener('click', expander2);
 imagenkachen.addEventListener('click', expander3);
 imagenLhotse.addEventListener('click', expander4);
+acercaDe1.addEventListener('click', cambiarAcercaDe1);
+acercaDe2.addEventListener('click', cambiarAcercaDe2);
+acercaDe3.addEventListener('click', cambiarAcercaDe3);
+acercaDe4.addEventListener('click', cambiarAcercaDe4);
 
 function expander1(){
     const isExpander2Open= imagenK2.classList.contains('expander');
@@ -85,4 +99,32 @@ function expander4(){
         imagenkachen.classList.toggle('expander');
     }
     imagenLhotse.classList.toggle('expander');
+}
+
+function cambiarAcercaDe1(){
+    mountainTallMexico.classList.add('inactive');
+    mountainTallWorld.classList.add('inactive');
+    escaladoresMasGrandes.classList.add('inactive');
+    queEsSenderismo.classList.remove('inactive');
+}
+
+function cambiarAcercaDe2(){
+    mountainTallMexico.classList.remove('inactive');
+    mountainTallWorld.classList.add('inactive');
+    escaladoresMasGrandes.classList.add('inactive');
+    queEsSenderismo.classList.add('inactive');
+}
+
+function cambiarAcercaDe3(){
+    mountainTallMexico.classList.add('inactive');
+    mountainTallWorld.classList.remove('inactive');
+    escaladoresMasGrandes.classList.add('inactive');
+    queEsSenderismo.classList.add('inactive');
+}
+
+function cambiarAcercaDe4(){
+    mountainTallMexico.classList.add('inactive');
+    mountainTallWorld.classList.add('inactive');
+    escaladoresMasGrandes.classList.remove('inactive');
+    queEsSenderismo.classList.add('inactive');
 }
