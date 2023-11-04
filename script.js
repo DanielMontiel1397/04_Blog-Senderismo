076da618062cd384d632b8637d675956696c2ccd
Daniel Montiel <danielponce1397@gmail.com>
2023-11-03 20:29:59 -0600

    Se expanden todas las imagenes al hacer click

--- script.js
+++ script.js
@@ -1,8 +1,88 @@
-const imagenMountains= document.querySelector('.imagenMountainTall');
+const imagenEverest= document.querySelector('.imageEverest');
+const imagenK2= document.querySelector('.imageK2');
+const imagenkachen= document.querySelector('.imageKachen');
+const imagenLhotse= document.querySelector('.imageLhotse');
 
-imagenMountains.addEventListener('click', expander);
+imagenEverest.addEventListener('click', expander1);
+imagenK2.addEventListener('click', expander2);
+imagenkachen.addEventListener('click', expander3);
+imagenLhotse.addEventListener('click', expander4);
 
-function expander(){
-    imagenMountains.classList.toggle('expander');
-    console.log("se agrego");
+function expander1(){
+    const isExpander2Open= imagenK2.classList.contains('expander');
+    const isExpander3Open= imagenkachen.classList.contains('expander');
+    const isExpander4Open= imagenLhotse.classList.contains('expander');
+
+    if(isExpander2Open){
+        imagenK2.classList.toggle('expander');
+    }
+
+    if(isExpander3Open){
+        imagenkachen.classList.toggle('expander');
+    }
+
+    if(isExpander4Open){
+        imagenLhotse.classList.toggle('expander');
+    }
+
+    imagenEverest.classList.toggle('expander');
+}
+
+function expander2(){
+    const isExpander1Open= imagenEverest.classList.contains('expander');
+    const isExpander3Open= imagenkachen.classList.contains('expander');
+    const isExpander4Open= imagenLhotse.classList.contains('expander');
+
+    if(isExpander1Open){
+        imagenEverest.classList.toggle('expander');
+    }
+
+    if(isExpander3Open){
+        imagenkachen.classList.toggle('expander');
+    }
+
+    if(isExpander4Open){
+        imagenLhotse.classList.toggle('expander');
+    }
+
+    imagenK2.classList.toggle('expander');
+}
+
+function expander3(){
+    const isExpander1Open= imagenEverest.classList.contains('expander');
+    const isExpander2Open= imagenK2.classList.contains('expander');
+    const isExpander4Open= imagenLhotse.classList.contains('expander');
+
+    if(isExpander1Open){
+        imagenEverest.classList.toggle('expander');
+    }
+
+    if(isExpander2Open){
+        imagenK2.classList.toggle('expander');
+    }
+
+    if(isExpander4Open){
+        imagenLhotse.classList.toggle('expander');
+    }
+
+    imagenkachen.classList.toggle('expander');
+}
+
+function expander4(){
+    const isExpander1Open= imagenEverest.classList.contains('expander');
+    const isExpander2Open= imagenK2.classList.contains('expander');
+    const isExpander3Open= imagenkachen.classList.contains('expander');
+
+    if(isExpander1Open){
+        imagenEverest.classList.toggle('expander');
+    }
+
+    if(isExpander2Open){
+        imagenK2.classList.toggle('expander');
+    }
+
+    if(isExpander3Open){
+        imagenkachen.classList.toggle('expander');
+    }
+    imagenLhotse.classList.toggle('expander');
 }
